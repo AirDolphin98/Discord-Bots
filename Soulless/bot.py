8fadd380f462df7c4ef7a0b17b7ff0ddff9f3a9d
AirDolphin98 <completeideal@gmail.com>
2025-08-01 10:53:06 -0700

    adjustments

--- Soulless/bot.py
+++ Soulless/bot.py
@@ -644,7 +644,7 @@ async def on_member_update(before, after): #for spam accounts joining in Soul Sa
     sus_colors = {797228323192700948, 797227641794461716, 797219806574149672, 804928191315705868, 797220234012262460} #Light Slate Blue, Pig Pink, Electric Indigo, Dark Violet, Electric Purple
     sus_clubs = {822853677093879838, 875840284947259412, 826982479919317013, 812293377956773918, 810822858442801172, 810822856853946408, 810822851748823040, 810822854018203658} # Minecraft, VC, various Sanctums
     bots_channel = after.guild.get_channel(793737732391698453) # bots
-    if len(sus_colors.intersection(roles_set)) > 1 and len(sus_clubs.intersection(roles_set)) > 2 and len(roles_set) - len(sus_colors.union(sus_clubs).intersection(roles_set)) < 3 and datetime.now(datetime_module.timezone.utc) - after.joined_at < timedelta(minutes=3):
+    if len(sus_colors.intersection(roles_set)) > 1 and len(sus_clubs.intersection(roles_set)) > 2 and len(roles_set) - len(sus_colors.union(sus_clubs).intersection(roles_set)) < 3 and datetime.now(datetime_module.timezone.utc) - after.joined_at < timedelta(hours=1):
         await after.ban(reason="detected spam role pattern")
         await bots_channel.send(f"Banned {after.mention} who joined <t:{round(after.joined_at.timestamp())}:f> for sus roles select pattern :dancer:\nRoles: {' | '.join([r.name for r in after.roles][1:])}")
 
